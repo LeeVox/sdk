2543167a54c3b0d9c03f0506e2dae7a845d74b5e
Dat Le <lethanhdat@gmail.com>
2018-11-21 17:58:48 +0700

    Updated unit test cases for String.Equals() to ignore OS differences.
    Change-Id: I66b3b45c343ba480ba0c73960eecb540862f4a86

--- LeeVox.Sdk.Test/Text/StringExtensionTests.cs
+++ LeeVox.Sdk.Test/Text/StringExtensionTests.cs
@@ -10,14 +10,19 @@ namespace LeeVox.Sdk.Test
     [TestClass]
     public class StringExtensionTests
     {
-        static readonly string STRING_1 = "encyclopædia";
-        static  readonly string STRING_1_UPPER = STRING_1.ToUpper();
-        static  readonly string STRING_1_SPACES = $"\t {STRING_1}\r\n ";
-        static  readonly string STRING_1_SPACES_UPPER = STRING_1_SPACES.ToUpper();
-        static  readonly string STRING_2 = "encyclopaedia";
-        static  readonly string STRING_2_UPPER = STRING_2.ToUpper();
-        static  readonly string STRING_2_SPACES = $"\r\n {STRING_2}\t ";
-        static  readonly string STRING_2_SPACES_UPPER = STRING_2_SPACES.ToUpper();
+        const string US = "en-US";
+        const string SE = "se-SE";
+
+        const string NULL_STRING_1 = null;
+        const string NULL_STRING_2 = null;
+        const string STRING_1 = "EncycLopædia";
+        const string STRING_1_UPPER = "ENCYCLOPÆDIA";
+        const string STRING_1_SPACES = "\t EncycLopædia\r\n ";
+        const string STRING_1_SPACES_UPPER = "\t ENCYCLOPÆDIA\r\n ";
+        const string STRING_2 = "EncycLopaedia";
+        const string STRING_2_LOWER = "encyclopaedia";
+        const string STRING_2_SPACES = "\r\n EncycLopaedia\t ";
+        const string STRING_2_SPACES_LOWER = "\r\n encyclopaedia\t ";
 
         static readonly char[] ALL_WHITESPACES = Enumerable.Range(0, ushort.MaxValue)
             .Where(c => {
@@ -60,77 +65,80 @@ namespace LeeVox.Sdk.Test
         }
 
         [TestMethod]
-        public void EqualsTests_se_SE()
+        [DataRow(US, STRING_1, STRING_1_UPPER)]
+        [DataRow(US, STRING_1, STRING_1_SPACES)]
+        [DataRow(US, STRING_1, STRING_1_SPACES_UPPER)]
+        [DataRow(US, STRING_1_SPACES, STRING_1_SPACES_UPPER)]
+        [DataRow(US, STRING_2, STRING_1)]
+        [DataRow(US, STRING_2_SPACES, STRING_1_UPPER)]
+        [DataRow(US, STRING_2_LOWER, STRING_1_SPACES_UPPER)]
+        [DataRow(US, STRING_2_SPACES_LOWER, STRING_1_SPACES_UPPER)]
+        
+        [DataRow(SE, STRING_1, STRING_1_UPPER)]
+        [DataRow(SE, STRING_1, STRING_1_SPACES)]
+        [DataRow(SE, STRING_1, STRING_1_SPACES_UPPER)]
+        [DataRow(SE, STRING_1_SPACES, STRING_1_SPACES_UPPER)]
+        [DataRow(SE, STRING_2, STRING_1)]
+        [DataRow(SE, STRING_2_SPACES, STRING_1_UPPER)]
+        [DataRow(SE, STRING_2_LOWER, STRING_1_SPACES_UPPER)]
+        [DataRow(SE, STRING_2_SPACES_LOWER, STRING_1_SPACES_UPPER)]
+        public void EqualsTests_NotNullString(string cultureName, string a, string b)
         {
             var currentCultureName = CultureInfo.CurrentCulture.Name;
-            CultureInfo.CurrentCulture = new CultureInfo("se-SE");
+            CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+            
+            Assert.AreEqual(string.Equals(a, b, StringComparison.Ordinal), a.OrdinalEquals(b));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.Ordinal), a.OrdinalEquals(b, false));
+            Assert.AreEqual(string.Equals(a.ToUpper(), b.ToLower(), StringComparison.OrdinalIgnoreCase), a.OrdinalEquals(b, true));
 
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_2));
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_1_UPPER));
-            Assert.IsTrue(STRING_1_UPPER.OrdinalEquals(STRING_1, true));
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_2_UPPER, true));
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b));
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b, false));
+            Assert.AreEqual(string.Equals(a.ToUpper().Trim(), b.Trim(), StringComparison.CurrentCultureIgnoreCase), a.EqualsIgnoreSpaces(b, true));
+            Assert.AreEqual(string.Equals(a.Trim(), b.ToLower().Trim(), StringComparison.CurrentCultureIgnoreCase), a.EqualsIgnoreSpaces(b, true));
 
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_1_SPACES));
-            Assert.IsFalse(STRING_1.EqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1.EqualsIgnoreSpaces(STRING_2_SPACES));
-            Assert.IsFalse(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_SPACES));
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b, StringComparison.CurrentCulture));
+            Assert.AreEqual(string.Equals(a.ToUpper().Trim(), b.ToLower().Trim(), StringComparison.CurrentCultureIgnoreCase), a.EqualsIgnoreSpaces(b, StringComparison.CurrentCultureIgnoreCase));
+#if NETCOREAPP2_0_OR_ABOVE || NETSTANDARD2_0_OR_ABOVE
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.InvariantCulture), a.EqualsIgnoreSpaces(b, StringComparison.InvariantCulture));
+            Assert.AreEqual(string.Equals(a.ToUpper().Trim(), b.ToLower().Trim(), StringComparison.InvariantCultureIgnoreCase), a.EqualsIgnoreSpaces(b, StringComparison.InvariantCultureIgnoreCase));
+#endif
 
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_1_SPACES_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES_UPPER.EqualsIgnoreSpaces(STRING_2, true));
-            Assert.IsFalse(STRING_1_UPPER.EqualsIgnoreSpaces(STRING_2_SPACES, true));
-            Assert.IsFalse(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_SPACES_UPPER, true));
-
-            Assert.IsTrue(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_1_SPACES));
-            Assert.IsFalse(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES));
-
-            Assert.IsTrue(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_1_SPACES_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES_UPPER.OrdinalEqualsIgnoreSpaces(STRING_2, true));
-            Assert.IsFalse(STRING_1_UPPER.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES, true));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES_UPPER, true));
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.Ordinal), a.OrdinalEqualsIgnoreSpaces(b));
+            Assert.AreEqual(string.Equals(a.Trim(), b.Trim(), StringComparison.Ordinal), a.OrdinalEqualsIgnoreSpaces(b, false));
+            Assert.AreEqual(string.Equals(a.ToUpper().Trim(), b.Trim(), StringComparison.OrdinalIgnoreCase), a.OrdinalEqualsIgnoreSpaces(b, true));
+            Assert.AreEqual(string.Equals(a.Trim(), b.ToLower().Trim(), StringComparison.OrdinalIgnoreCase), a.OrdinalEqualsIgnoreSpaces(b, true));
 
             CultureInfo.CurrentCulture = new CultureInfo(currentCultureName);
         }
 
         [TestMethod]
-        public void EqualsTests_en_US()
+        [DataRow(US, NULL_STRING_1, NULL_STRING_2)]
+        [DataRow(US, NULL_STRING_2, STRING_2)]
+        [DataRow(US, STRING_1, NULL_STRING_1)]
+
+        [DataRow(SE, NULL_STRING_1, NULL_STRING_2)]
+        [DataRow(SE, NULL_STRING_2, STRING_2)]
+        [DataRow(SE, STRING_1, NULL_STRING_1)]
+        public void EqualsTests_WithNull(string cultureName, string a, string b)
         {
             var currentCultureName = CultureInfo.CurrentCulture.Name;
-            CultureInfo.CurrentCulture = new CultureInfo("en-US");
+            CultureInfo.CurrentCulture = new CultureInfo(cultureName);
+            
+            Assert.AreEqual(string.Equals(a, b, StringComparison.Ordinal), a.OrdinalEquals(b));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.Ordinal), a.OrdinalEquals(b, false));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.OrdinalIgnoreCase), a.OrdinalEquals(b, true));
 
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_2));
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_1_UPPER));
-            Assert.IsTrue(STRING_1_UPPER.OrdinalEquals(STRING_1, true));
-            Assert.IsFalse(STRING_1.OrdinalEquals(STRING_2_UPPER, true));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b, false));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.CurrentCulture), a.EqualsIgnoreSpaces(b, StringComparison.CurrentCulture));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.CurrentCultureIgnoreCase), a.EqualsIgnoreSpaces(b, StringComparison.CurrentCultureIgnoreCase));
+#if NETCOREAPP2_0_OR_ABOVE || NETSTANDARD2_0_OR_ABOVE
+            Assert.AreEqual(string.Equals(a, b, StringComparison.InvariantCulture), a.EqualsIgnoreSpaces(b, StringComparison.InvariantCulture));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.InvariantCultureIgnoreCase), a.EqualsIgnoreSpaces(b, StringComparison.InvariantCultureIgnoreCase));
+#endif
 
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_1_SPACES));
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_2));
-            Assert.IsTrue(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2));
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_2_SPACES));
-            Assert.IsTrue(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_SPACES));
-
-            Assert.IsTrue(STRING_1.EqualsIgnoreSpaces(STRING_1_SPACES_UPPER, true));
-            Assert.IsTrue(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_UPPER, true));
-            Assert.IsTrue(STRING_1_SPACES_UPPER.EqualsIgnoreSpaces(STRING_2, true));
-            Assert.IsTrue(STRING_1_UPPER.EqualsIgnoreSpaces(STRING_2_SPACES, true));
-            Assert.IsTrue(STRING_1_SPACES.EqualsIgnoreSpaces(STRING_2_SPACES_UPPER, true));
-
-            Assert.IsTrue(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_1_SPACES));
-            Assert.IsFalse(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2));
-            Assert.IsFalse(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES));
-
-            Assert.IsTrue(STRING_1.OrdinalEqualsIgnoreSpaces(STRING_1_SPACES_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_UPPER, true));
-            Assert.IsFalse(STRING_1_SPACES_UPPER.OrdinalEqualsIgnoreSpaces(STRING_2, true));
-            Assert.IsFalse(STRING_1_UPPER.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES, true));
-            Assert.IsFalse(STRING_1_SPACES.OrdinalEqualsIgnoreSpaces(STRING_2_SPACES_UPPER, true));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.Ordinal), a.OrdinalEqualsIgnoreSpaces(b));
+            Assert.AreEqual(string.Equals(a, b, StringComparison.OrdinalIgnoreCase), a.OrdinalEqualsIgnoreSpaces(b, false));
 
             CultureInfo.CurrentCulture = new CultureInfo(currentCultureName);
         }
